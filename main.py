388956377ec81ca728a95c24902264550247c9fd
student <student@example.com>
2026-03-11 18:47:41 +0300

    sdjha

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#main.py
+# main.py
 print("Добро пожаловать в калькулятор!")
 def add(a, b):
     return a + b
+def subtract(a, b):   # <-- Новая функция
+    return a - b
 if __name__ == "__main__":
     print(f"2 + 2 = {add(2, 2)}")
+    print(f"10 - 3 = {subtract(10, 3)}") # <-- Новая строка
 
